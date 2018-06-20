c03a588bcd1d5c7165707cd6051d146963d21157
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-20 12:08:05 -0400

    updated to solution files

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,15 @@
 'use strict';
-/* global shoppingList, cuid */
-
-// eslint-disable-next-line no-unused-vars
-const store = {
-  items: [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
-  ],
-  hideCheckedItems: false,
-  searchTerm: ''
-};
+/* global shoppingList, store, Item, api $ */
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
 
+store.items.push(Item.create('apples'));
+
 api.getItems(function(data) {
   console.log(data);
 });
 
 console.log(api.BASE_URL);
-
