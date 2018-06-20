6d75bf7af76ac72f7762d1c29abf1fc6b51a6bdc
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-20 11:48:02 -0400

    getitems works

--- scripts/api.js
+++ scripts/api.js
@@ -1,11 +1,15 @@
 'use strict';
 const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/mitchjenn';
+  
   const getItems = function(callback){
     callback('api module works!');
+
+    //$.getJSON(`${BASE_URL}/items`, callback);
   };
   return {
-      getItems,
+    getItems,
   };
+
 }());
 
