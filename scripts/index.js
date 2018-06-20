68dd5304c65c99a8f14cdb069e425562210925aa
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-20 10:43:25 -0500

    added api.js

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,9 @@ $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
+
+api.getItems(function(data) {
+  console.log(data);
+});
+
+console.log(api.BASE_URL);
