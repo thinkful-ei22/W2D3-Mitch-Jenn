678eb5d9912ad81e769ec797ff38103db8356786
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-20 15:22:46 -0400

    step 7 complete

--- scripts/store.js
+++ scripts/store.js
@@ -11,24 +11,30 @@ const store = (function(){
     return this.items.find(item => item.id === id);
   };
 
-  const findAndToggleChecked = function(id) {
-    const item = this.findById(id);
-    item.checked = !item.checked;
+  const findAndUpdate = function(id, newData) {
+    let item = this.findById(id);
+    console.log(newData);
+    item = Object.assign(item, newData);
   };
+
+  // const findAndToggleChecked = function(id) {
+  //   const item = this.findById(id);
+  //   item.checked = !item.checked;
+  // };
 
   const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
   };
 
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch(e) {
-      console.log('Cannot update name: ' + e.message);
-    }
-  };
+  // const findAndUpdateName = function(id, name) {
+  //   try {
+  //     Item.validateName(name);
+  //     const item = this.findById(id);
+  //     item.name = name;
+  //   } catch(e) {
+  //     console.log('Cannot update name: ' + e.message);
+  //   }
+  // };
 
   const toggleCheckedFilter = function() {
     this.hideCheckedItems = !this.hideCheckedItems;
@@ -45,11 +51,10 @@ const store = (function(){
 
     addItem,
     findById,
-    findAndToggleChecked,
+    findAndUpdate,
     findAndDelete,
-    findAndUpdateName,
     toggleCheckedFilter,
     setSearchTerm,
   };
   
-}())
+}());
